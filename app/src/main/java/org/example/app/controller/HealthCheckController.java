60cfc3e938d3b47a9fd38a4615b982373675fa73
hasya1993 <165818152+hasya1993@users.noreply.github.com>
2024-04-07 18:21:50 +0300

    Revert "Revert "Configure module app as Spring boot project""

---
+++ app/src/main/java/org/example/app/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package org.example.app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.noContent().build();
+    }
+}
